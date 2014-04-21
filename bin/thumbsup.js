70c77fd36b317775553647114768abd2e318deb4
rprieto <choicesmade@gmail.com>
2014-04-21 21:48:33 +1000

    Fix binary

--- bin/thumbsup.js
+++ bin/thumbsup.js
@@ -5,15 +5,15 @@ var index = require('../src/index');
 
 program
   .version('0.0.1')
-  .option('-i, --input [path]', 'Path to the folder with all photos/videos')
-  .option('-o, --output [path]', 'Output path for the static website')
-  .option('-p, --media-prefix [url]', 'URL prefix for all media')
-  .option('-s, --size [pixels]', 'Thumbnail size in pixels (square)', '100')
+  .option('-i, --input <path>', 'Path to the folder with all photos/videos')
+  .option('-o, --output <path>', 'Output path for the static website')
+  .option('-t, --thumb-size [pixels]', 'Thumbnail size in pixels (square)')
+  .option('-l, --large-size [pixels]', 'Fullscreen size in pixels (square)')
   .parse(process.argv);
 
 index.build({
   input: program.input,
   output: program.output,
-  mediaPrefix: program.mediaPrefix,
-  size: program.size
+  thumbSize: program.thumbSize,
+  largeSize: program.largeSize
 });
