f66208e052ce618da0da5dd6c78ff06f9fcd7532
Romain <asyncadventures@gmail.com>
2017-11-29 21:11:12 +1100

    New options for the {{date}} helper: custom date format + how long ago

--- src/website/helpers/date.js
+++ src/website/helpers/date.js
@@ -4,7 +4,18 @@ const moment = require('moment')
   Render a date in a legible format
   Usage:
     {{date taken}}
+    {{date taken "MMMM YYYY"}}
+    {{date taken "ago"}}
 */
-module.exports = (date) => {
-  return moment(date).format('DD MMM YYYY')
+module.exports = (date, format) => {
+  if (typeof format !== 'string') {
+    // default format
+    return moment(date).format('DD MMM YYYY')
+  } else if (format === 'ago') {
+    // some time ago
+    return moment(date).fromNow()
+  } else {
+    // custom format
+    return moment(date).format(format)
+  }
 }
--- test/website/helpers/date.spec.js
+++ test/website/helpers/date.spec.js
@@ -1,13 +1,29 @@
 const date = require('../../../src/website/helpers/date')
 const handlebars = require('handlebars')
+const moment = require('moment')
 const should = require('should/as-function')
 
 describe('Handlebars helpers: date', () => {
   handlebars.registerHelper('date', date)
 
-  it('renders a date as DD MMM YYYY', () => {
+  it('renders a date as DD MMM YYYY by default', () => {
     const template = handlebars.compile(`<p>{{date taken}}</p>`)
     const res = template({taken: new Date(2017, 10, 27)}) // month is 0-based
     should(res).eql('<p>27 Nov 2017</p>')
+  })
+
+  it('renders a date with a custom format', () => {
+    const template = handlebars.compile(`<p>{{date taken "MMMM YYYY"}}</p>`)
+    const res = template({taken: new Date(2017, 10, 27)}) // month is 0-based
+    should(res).eql('<p>November 2017</p>')
+  })
+
+  it('renders a date as <time ago>', () => {
+    const template = handlebars.compile(`<p>{{date taken "ago"}}</p>`)
+    const data = {taken: new Date(2017, 10, 27)}  // month is 0-based
+    const res = template(data)
+    const expected = moment(data.taken).fromNow()
+    should(res).match(/<p>(.*) ago<\/p>/)
+    should(res).eql(`<p>${expected}</p>`)
   })
 })
