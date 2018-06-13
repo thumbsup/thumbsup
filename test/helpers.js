6d94b6339f9b82c07001cdeeba607473b6ab733b
Romain <asyncadventures@gmail.com>
2018-06-14 00:02:03 +0200

    chore(tests): handle rejected promises during tests

--- test/helpers.js
+++ test/helpers.js
@@ -4,3 +4,9 @@ const requireAll = require('require-all')
 // require all source code
 // so that the coverage report is accurate
 requireAll(path.join(__dirname, '..', 'src'))
+
+// capture all unhandled rejected promises
+// and ensure the current test fails
+process.on('unhandledRejection', err => {
+  throw err
+})
