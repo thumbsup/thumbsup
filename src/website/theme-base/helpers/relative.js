c414b93125d7cfa7af84a225eb21b925271a68cd
Andrew McOlash <amcolash@gmail.com>
2019-02-10 15:13:35 -0800

    Add in escaping of quotes for relative paths

--- src/website/theme-base/helpers/relative.js
+++ src/website/theme-base/helpers/relative.js
@@ -1,6 +1,19 @@
 const path = require('path')
 
-module.exports = (target, options) => {
-  const albumPath = options.data.root.album.path
-  return path.relative(path.dirname(albumPath), target)
+module.exports = (target, escape, options) => {
+    // Allow escape to be an optional parameter
+    if (typeof escape !== "boolean") {
+        options = escape;
+        escape = false;
+    }
+
+    const albumPath = options.data.root.album.path
+    var relative = path.relative(path.dirname(albumPath), target);
+
+    // Escape single quotes
+    if (escape) {
+        return relative.replace(/\'/g, "%27");
+    } else {
+        return relative;
+    }
 }
