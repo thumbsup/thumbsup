ed97c66e8db3dfa67df3a24dfe9b0ffd71946ba8
Andrew McOlash <amcolash@gmail.com>
2019-02-10 15:23:36 -0800

    Fix lint errors

--- src/website/theme-base/helpers/relative.js
+++ src/website/theme-base/helpers/relative.js
@@ -1,21 +1,21 @@
 const path = require('path')
 
 module.exports = (target, escape, options) => {
-    // Allow escape to be an optional parameter
-    if (typeof escape !== "boolean") {
-        options = escape;
-        escape = false;
-    }
+  // Allow escape to be an optional parameter
+  if (typeof escape !== 'boolean') {
+    options = escape
+    escape = false
+  }
 
-    const albumPath = options.data.root.album.path
-    var relative = path.relative(path.dirname(albumPath), target);
+  const albumPath = options.data.root.album.path
+  var relative = path.relative(path.dirname(albumPath), target)
 
-    // Escape single quotes
-    if (escape) {
-      var escaped = relative.replace(/\'/g, "%27");
-      escaped = escaped.replace(/\"/g, "%22");
-      return escaped;
-    } else {
-        return relative;
-    }
+  // Escape single quotes
+  if (escape) {
+    var escaped = relative.replace(/'/g, '%27')
+    escaped = escaped.replace(/"/g, '%22')
+    return escaped
+  } else {
+    return relative
+  }
 }
