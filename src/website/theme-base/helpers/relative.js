4b7b3168b3d213beb7053132cf11e426e4f06307
Andrew McOlash <amcolash@gmail.com>
2019-02-14 00:39:10 -0800

    Always escape relative paths

--- src/website/theme-base/helpers/relative.js
+++ src/website/theme-base/helpers/relative.js
@@ -1,21 +1,11 @@
 const path = require('path')
 
-module.exports = (target, escape, options) => {
-  // Allow escape to be an optional parameter
-  if (typeof escape !== 'boolean') {
-    options = escape
-    escape = false
-  }
-
+module.exports = (target, options) => {
   const albumPath = options.data.root.album.path
   var relative = path.relative(path.dirname(albumPath), target)
 
-  // Escape single quotes
-  if (escape) {
-    var escaped = relative.replace(/'/g, '%27')
-    escaped = escaped.replace(/"/g, '%22')
-    return escaped
-  } else {
-    return relative
-  }
+  // Escape single/double quotes
+  var escaped = relative.replace(/'/g, '%27')
+  escaped = escaped.replace(/"/g, '%22')
+  return escaped
 }
