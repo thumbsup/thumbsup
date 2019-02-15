2e875e7448ecdea169845d70e3bbc604717e218a
Romain <asyncadventures@gmail.com>
2019-02-15 21:04:57 +0100

    test(theme): add tests for escaping of single and double quotes

--- src/website/theme-base/helpers/relative.js
+++ src/website/theme-base/helpers/relative.js
@@ -2,10 +2,7 @@ const path = require('path')
 
 module.exports = (target, options) => {
   const albumPath = options.data.root.album.path
-  var relative = path.relative(path.dirname(albumPath), target)
-
+  const relative = path.relative(path.dirname(albumPath), target)
   // Escape single/double quotes
-  var escaped = relative.replace(/'/g, '%27')
-  escaped = escaped.replace(/"/g, '%22')
-  return escaped
+  return relative.replace(/'/g, '%27').replace(/"/g, '%22')
 }
