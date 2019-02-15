11da4850c8d4e9e62bc16e72c2fd9ac43ef2d24d
Andrew McOlash <amcolash@gmail.com>
2019-02-14 22:07:19 -0800

    Add in escaping of quotes for relative paths (#145)
    * Add in escaping of quotes for relative paths
    
    * Add double quote escaping
    
    * Fix lint errors
    
    * Always escape relative paths

--- src/website/theme-base/helpers/relative.js
+++ src/website/theme-base/helpers/relative.js
@@ -2,5 +2,10 @@ const path = require('path')
 
 module.exports = (target, options) => {
   const albumPath = options.data.root.album.path
-  return path.relative(path.dirname(albumPath), target)
+  var relative = path.relative(path.dirname(albumPath), target)
+
+  // Escape single/double quotes
+  var escaped = relative.replace(/'/g, '%27')
+  escaped = escaped.replace(/"/g, '%22')
+  return escaped
 }
